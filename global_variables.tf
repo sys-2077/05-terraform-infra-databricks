df11ca103633cd1674286859f6d56ca0142e9871
sys-2077 <elevatedappsmethod@gmail.com>
2024-03-27 16:09:01 -0400

    terraform

--- global_variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  type        = string
-  description = "El nombre del grupo de recursos donde se crearán los recursos."
-}
-
-variable "location" {
-  type        = string
-  description = "Region en Azure."
-}
