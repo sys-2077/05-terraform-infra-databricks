9314dac47c5f982b91d9fa71085236136e0bbfba
sys-2077 <elevatedappsmethod@gmail.com>
2024-03-31 22:54:06 -0400

    add cluster 3

--- terraform-db-dev/main.tf
+++ terraform-db-dev/main.tf
@@ -2,13 +2,6 @@ resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
 }
-resource "azurerm_storage_account" "this" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = azurerm_resource_group.this.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
 
 resource "azurerm_databricks_workspace" "this" {
   name                        = "db-workspace"
